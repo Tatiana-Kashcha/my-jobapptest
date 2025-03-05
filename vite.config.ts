017c980bc01ffce8c01c36f3ef627ed748ab933f
Tatiana Kashcha <tkashcha@gmail.com>
2025-03-05 23:46:21 +0200

    upd deploy in vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-jobapptest/",
+  base: "/",
+  // base: "/my-jobapptest/", // коли деплой на gh-pages
 });
+
+// "homepage": "https://tatiana-kashcha.github.io/my-jobapptest/",
+// коли деплой на gh-pages,
+// "scripts": { "deploy": "gh-pages -d dist",
+// це для package.json файла
